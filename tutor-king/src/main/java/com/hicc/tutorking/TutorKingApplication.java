9547b4fea7826910cb16166065a1451e0bd058b2
JSHWJ <bogum1029@gmail.com>
2022-07-21 00:56:02 +0900

    개발 환경 세팅

---
+++ tutor-king/src/main/java/com/hicc/tutorking/TutorKingApplication.java
@@ -0,0 +1,13 @@
+package com.hicc.tutorking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorKingApplication.class, args);
+	}
+
+}
---
+++ tutor-king/src/main/java/com/hicc/tutorking/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.hicc.tutorking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value = "/")
+    public String helloWorld() {
+        return "Helloworld";
+    }
+}
