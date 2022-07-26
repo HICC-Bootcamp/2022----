7892b77cf9c9226be6955435a6cf0744df4f90c2
조예지 <yejee1018@naver.com>
2022-07-26 21:09:40 +0900

    version 1
    버전 1

--- tutor-king/src/main/java/com/hicc/tutorking/service/AccountService.java
+++ tutor-king/src/main/java/com/hicc/tutorking/service/AccountService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class AccountService {
+public class AccountService implements  UserDetailsService{
     private final AccountRepository accountRepository;
 
     public Account saveAccount(Account account) {
@@ -18,10 +18,29 @@ public class AccountService {
         return accountRepository.save(account);
     }
 
+
     private void validateDuplicateAccount(Account account) {
         Account findAccount = accountRepository.findByEmail(account.getEmail());
         if (findAccount != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Account account = accountRepository.findByEmail(email);
+
+        if(account == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()//User객체를 반환할 때, 이메일 비밀번호 타입을 파라미터로 넘겨줌
+                .username(account.getEmail())
+                .password(account.getPassword())
+                .roles(account.getRole().toString())
+                .build();
+    }
+
+
 }
