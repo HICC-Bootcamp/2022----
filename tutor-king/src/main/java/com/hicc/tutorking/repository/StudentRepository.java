bcef475b9f6dba5edc64bec0bae8e61f87ea3ad4
JSHWJ <bogum1029@gmail.com>
2022-08-03 04:35:35 +0900

    pr create

--- tutor-king/src/main/java/com/hicc/tutorking/repository/StudentRepository.java
+++ tutor-king/src/main/java/com/hicc/tutorking/repository/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.hicc.tutorking.repository;
 
+import com.hicc.tutorking.entity.Account;
 import com.hicc.tutorking.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    Student findByAccount(Account temp);
 
 }
