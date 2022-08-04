4d587e29435de7424aa1f524795bf20a79f54d19
JSHWJ <bogum1029@gmail.com>
2022-08-05 01:25:55 +0900

    todo 추가

--- tutor-king/src/main/java/com/hicc/tutorking/entity/Connection.java
+++ tutor-king/src/main/java/com/hicc/tutorking/entity/Connection.java
@@ -24,6 +24,11 @@ public class Connection {
     private String teacherEmail;
     private Boolean teacherReply;
 
+    // TODO: 자신의 이메일 주소(학생)를 principal로 받아와서, 이걸 connection repository 에서 찾는다
+    // TODO: 그래서 그 findByEmail 로 찾은 connection repository 에서 teacherEmail을 찾아서 그걸로
+    //  teacher repository를 찾아서 그 레포안에 있는 선생님의 정보를 프론트엔드에 전달해준다
+    // TODO: 그래서 그 findByEmail 로 찾은 connection repository 에서 teacherReply에 뭐가 담겨있는지를 프론트엔드에 전달해준다
+
     public static Connection createConnection(String studentEmail, ConnectionDto connectionDto) {
         Connection connection = new Connection();
         connection.setStudentEmail(studentEmail);
